21f50d1af10d3d5ddc9cec3e11903b227a339be7
HavoK <HavoK@DESKTOP-37GRLFQ>
2026-01-22 01:15:23 +0000

    Added FreeType for better quality font rendering

--- example_win32_directx12/settings.h
+++ example_win32_directx12/settings.h
@@ -1,6 +1,10 @@
+#include "hvkgui.h"
+
 class c_user_settings
 {
     public:
+
+    bool show_style_editor = false;
 
     struct {
         struct {
@@ -13,3 +17,14 @@ class c_user_settings
     } style;
 
 };
+
+inline c_user_settings* user_settings = new c_user_settings();
+
+void hvkgui_apply_user_settings()
+{
+    HvkGuiStyle& style = HvkGui::GetStyle();
+    style.Colors[HvkGuiCol_WindowBg].x = user_settings->style.color.r;
+    style.Colors[HvkGuiCol_WindowBg].y = user_settings->style.color.g;
+    style.Colors[HvkGuiCol_WindowBg].z = user_settings->style.color.b;
+    style.Colors[HvkGuiCol_WindowBg].w = user_settings->style.color.a;
+}
